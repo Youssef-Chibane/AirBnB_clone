76b2e4c0bb576c0b8cd478b9ee47415e77d50388
Youssef-Chibane <chib4ne.youssef@gmail.com>
2024-01-14 10:22:20 +0100

    what?

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+
 """ Class user that inherits from base model"""
 
 from models.base_model import BaseModel
-
 
 class User(BaseModel):
     """ Class user that inherits from base model """
